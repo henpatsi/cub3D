aa123ec1c6deec8b83da42df7cb7f478b61992e2
Henri Patsi <hpatsi@student.hive.fi>
2024-05-07 12:55:10 +0300

    cleaned up main

--- srcs/main.c
+++ srcs/main.c
@@ -6,45 +6,49 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 08:54:42 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/05/07 10:43:21 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/05/07 12:54:46 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int	test_game(t_map *map, char **argv)
+int	initialize_game(t_map *map, char *map_path)
 {
 	map->mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "cub3D", false);
 	if (map->mlx == 0)
-	{
-		ft_putstr_fd("Failed to init mlx\n", 2);
-		return (1);
-	}
-	if (load_map(map, argv[1]) == -1)
+		return (-1);
+	if (load_map(map, map_path) == -1)
 	{
 		mlx_terminate(map->mlx);
-		return (1);
+		return (-1);
 	}
 	if (init_visuals(map) == -1)
 	{
 		mlx_terminate(map->mlx);
-		return (1);
+		return (-1);
 	}
 	if (init_minimap(map) == -1)
 	{
 		mlx_terminate(map->mlx);
-		return (1);
+		return (-1);
 	}
 	mlx_set_cursor_mode(map->mlx, MLX_MOUSE_HIDDEN);
+	return (1);
+}
+
+int	run_game(t_map *map)
+{
 	mlx_cursor_hook(map->mlx, cursor_input_hook, map);
 	mlx_key_hook(map->mlx, key_hook, map);
-	mlx_loop_hook(map->mlx, keyboard_input_hook, map);
-	mlx_loop_hook(map->mlx, update_visuals_hook, map);
-	mlx_loop_hook(map->mlx, animation_hook, map);
+	if (!mlx_loop_hook(map->mlx, keyboard_input_hook, map))
+		return (-1);
+	if (!mlx_loop_hook(map->mlx, update_visuals_hook, map))
+		return (-1);
+	if (!mlx_loop_hook(map->mlx, animation_hook, map))
+		return (-1);
 	mlx_loop(map->mlx);
 	mlx_terminate(map->mlx);
-	free_all(map);
-	return (0);
+	return (1);
 }
 
 int	main(int argc, char **argv)
@@ -52,7 +56,13 @@ int	main(int argc, char **argv)
 	t_map	map;
 
 	validate_input(argc, argv, &map);
-	if (test_game(&map, argv))
+	if (initialize_game(&map, argv[1]) == -1)
 		return (1);
+	if (run_game(&map) == -1)
+	{
+		free_all(&map);
+		return (1);
+	}
+	free_all(&map);
 	return (0);
 }
