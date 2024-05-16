dcb0da8650ea86929c59426095dccc4cbe94390b
Henri Pätsi <patsihenri@gmail.com>
2024-05-16 21:56:44 +0300

    made load map close exit on error

--- srcs/load/load_map.c
+++ srcs/load/load_map.c
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 10:17:11 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/05/16 10:35:35 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/05/16 21:56:04 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,11 @@ int	load_map(t_map *map, char *map_filename)
 		exit(1);
 	}
 	if (close(map_fd) == -1)
+	{
 		perror("close() error");
+		free_textures(map->textures);
+		free_grid(map->grid);
+		exit(1);
+	}
 	return (1);
 }
